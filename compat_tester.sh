a6b73d305c528bb638596988a54737886c7b898b
henriquebritoM <henriquebrito667@gmail.com>
2026-05-27 21:39:46 -0300

    Added feature: Installation of LTP
    Added 3 installations methods:
    install: install ltp only if the ltp dir is not present
    update: freshly reinstalls ltp only if the local version is different.
    from th release version.
    force_reinstall: freshy reinstall ltp.

--- compat_tester.sh
+++ compat_tester.sh
@@ -8,18 +8,20 @@
 # 	Requirements:
 # 	- autoconf
 # 	- automake
-# 	- git 
 # 	- gmake
 # 	- m4
 # 	- pkgconf
 # 	- suse_gcc12-15.5
+# 	- curl
 # 	- module compat_linux enabled
+
+set -e
 
 # Directory where this script is located. Used as root for other paths.
 BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # Directory where the ltp tests will be cloned
-LTP_DIR="$BASE_DIR/ltp"
+LTP_DIR="${BASE_DIR}/ltp"
 
 # Base directory for the syscalls subdirectory inside ltp
 SYSCALL_DIR="${LTP_DIR}/testcases/kernel/syscalls"
@@ -34,14 +36,81 @@ BINARIES_DIR="${BASE_DIR}/bin"
 CC="/emul/linux/usr/bin/gcc-12"
 
 # If the script should recompile the tests
-FORCE_COMPILE="false"
+FORCE_COMPILE=0
+
+# Release version of the tests
+LTP_URL=$(curl -s https://api.github.com/repos/linux-test-project/ltp/releases/latest | \
+	grep "browser_download_url.*ltp-full-.*\.tar\.xz\"" | \
+	cut -d'"' -f4)
+
+# Latest release available for LTP
+LTP_RELEASE=$(echo "${LTP_URL}" | sed 's/https:.*\///; s/.tar.xz//')
+
+LTP_VERSION_FILE="${BASE_DIR}/ltp_version"
+
+if ! [ -e "${LTP_VERSION_FILE}" ]; then
+	echo 0 > "${LTP_VERSION_FILE}"
+fi
+
+# Currently installed LTP version
+LTP_CURRENT_VERSION="$(cat ltp_version)"
+
+is_ltp_latest() {
+	echo "checking if the latest version of LTP is being used"
+
+	# Not in the latest release
+	if [ "${LTP_CURRENT_VERSION}" != "${LTP_RELEASE}" ]; then
+		echo "Not in the latest version"
+		return 1
+	fi
+
+	echo "Already in the latest version"
+	return 0
+}
+
+clear_ltp() {
+	# clears some related vars
+	echo 0 > "${LTP_VERSION_FILE}"
+	LTP_CURRENT_VERSION=0
+
+	rm -rf "${LTP_DIR:?}" 
+}
+
+install_ltp() {
+
+	# LTP_DIR does not exist. Download the latest release and decompress it
+	if ! [ -e "${LTP_DIR}" ]; then
+		echo "Downloading the latest LTP release"
+
+		mkdir "${LTP_DIR}"
+		echo "${LTP_RELEASE}" > "ltp_version"
+		curl -OLs "${LTP_URL}"
+		tar -xf "${LTP_RELEASE}.tar.xz" -C "${LTP_DIR}" --strip-components=1
+		rm "${LTP_RELEASE}.tar.xz" # no reason to keep the tar
+	fi
+}
+
+force_ltp_reinstall() {
+	clear_ltp
+	install_ltp
+}
+
+update_ltp() {
+
+	if ! is_ltp_latest; then
+		force_ltp_reinstall	
+	fi
+}
 
 compile_tests() {
-	# todo
+	cd "${LTP_DIR}"
+
 	return
 }
 
 clean() {
+	clear_ltp_dir
 	rm -rf "${BINARIES_DIR:?}/*"
 }
 
+update_ltp
